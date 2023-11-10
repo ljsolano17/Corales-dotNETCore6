42ef90d5e78f6c69ed4e27867e2e1e3adc5843d6
ljsolano17 <ljsolano17@hotmail.com>
2023-11-09 18:03:21 -0600

    Add Solution.DAL

---
+++ BACK/Solution/Solution.DAL/Articulos.cs
@@ -0,0 +1,60 @@
+﻿using Solution.DAL.EF;
+using Solution.DAL.Repository;
+using Solution.DO.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using data = Solution.DO.Objects;
+
+namespace Solution.DAL
+{
+    public class Articulos : ICRUD<data.Articulos>
+    {
+        private RepositoryArticulos _repo = null;
+
+        public Articulos(SolutionDbContext solutionDbContext)
+        {
+            _repo = new RepositoryArticulos(solutionDbContext);
+        }
+
+        public void Delete(data.Articulos t)
+        {
+            _repo.Delete(t);
+            _repo.Commit();
+        }
+
+        public IEnumerable<data.Articulos> GetAll()
+        {
+            return _repo.GetAll();
+        }
+
+        public data.Articulos GetOneById(int id)
+        {
+            return _repo.GetOneById(id);
+        }
+
+        public void Insert(data.Articulos t)
+        {
+            _repo.Insert(t);
+            _repo.Commit();
+        }
+
+        public void Update(data.Articulos t)
+        {
+            _repo.Update(t);
+            _repo.Commit();
+        }
+        public async Task<IEnumerable<data.Articulos>> GetAllWithAsAsync()
+        {
+            return await _repo.GetAllWithAsAsync();
+            
+        }
+        public async Task<data.Articulos> GetOneByIdlWithAsAsync(int id)
+        {
+            return await _repo.GetOneByIdWithAsAsync(id);
+        }
+
+    }
+}
---
+++ BACK/Solution/Solution.DAL/Categorias.cs
@@ -0,0 +1,50 @@
+﻿using Solution.DAL.EF;
+using Solution.DAL.Repository;
+using Solution.DO.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using data = Solution.DO.Objects;
+
+namespace Solution.DAL
+{
+    public class Categorias : ICRUD<data.Categorias>
+    {
+        private Repository<data.Categorias> _repo = null;
+
+        public Categorias(SolutionDbContext solutionDbContext)
+        {
+            _repo = new Repository<data.Categorias>(solutionDbContext);
+        }
+
+        public void Delete(data.Categorias t)
+        {
+            _repo.Delete(t);
+            _repo.Commit();
+        }
+
+        public IEnumerable<data.Categorias> GetAll()
+        {
+            return _repo.GetAll();
+        }
+
+        public data.Categorias GetOneById(int id)
+        {
+            return _repo.GetOneById(id);
+        }
+
+        public void Insert(data.Categorias t)
+        {
+            _repo.Insert(t);
+            _repo.Commit();
+        }
+
+        public void Update(data.Categorias t)
+        {
+            _repo.Update(t);
+            _repo.Commit();
+        }
+    }
+}
