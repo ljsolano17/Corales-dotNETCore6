8a99123598a3edeca9c511b292488d6a39f7e47e
ljsolano17 <ljsolano17@hotmail.com>
2023-11-09 19:04:58 -0600

    Add BS Layer

---
+++ BACK/Solution/Solution.BS/Categorias.cs
@@ -0,0 +1,44 @@
+﻿using Solution.DAL.EF;
+using Solution.DO.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using data = Solution.DO.Objects;
+
+namespace Solution.BS
+{
+    public class Categorias : ICRUD<data.Categorias>
+    {
+        private SolutionDbContext context;
+        public Categorias(SolutionDbContext _context)
+        {
+            context = _context;
+        }
+        public void Delete(data.Categorias t)
+        {
+            new DAL.Categorias(context).Delete(t);
+        }
+
+        public IEnumerable<data.Categorias> GetAll()
+        {
+            return new DAL.Categorias(context).GetAll();
+        }
+
+        public data.Categorias GetOneById(int id)
+        {
+            return new DAL.Categorias(context).GetOneById(id);
+        }
+
+        public void Insert(data.Categorias t)
+        {
+            new DAL.Categorias(context).Insert(t);
+        }
+
+        public void Update(data.Categorias t)
+        {
+            new DAL.Categorias(context).Update(t);
+        }
+    }
+}
